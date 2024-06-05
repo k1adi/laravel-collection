8b0579507cab0668327236a7984cb8aae5164958
k1adi <rizkiadinugroho3@gmail.com>
2024-06-05 11:36:31 +0700

    implementation collection zipping

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -125,4 +125,38 @@ class CollectionTest extends TestCase
             'HR' => collect(['Nug'])
         ], $result->all());
     }
+
+    public function testZip()
+    {
+        $collection1 = collect([1, 2, 3]);
+        $collection2 = collect([4, 5, 6]);
+        $collection3 = $collection1->zip($collection2);
+
+        $this->assertEquals([
+            collect([1, 4]),
+            collect([2, 5]),
+            collect([3, 6])
+        ], $collection3->all());
+    }
+
+    public function testConcat()
+    {
+        $collection1 = collect([1, 2, 3]);
+        $collection2 = collect([4, 5, 6]);
+        $collection3 = $collection1->concat($collection2);
+
+        $this->assertEqualsCanonicalizing([1,2,3,4,5,6], $collection3->all());
+    }
+
+    public function testCombine()
+    {
+        $collection1 = collect(['name', 'country']);
+        $collection2 = collect(['Rizki', 'Indonesia']);
+        $collection3 = $collection1->combine($collection2);
+
+        $this->assertEquals([
+            'name' => 'Rizki',
+            'country' => 'Indonesia'
+        ], $collection3->all());
+    }
 }
