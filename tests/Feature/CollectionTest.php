9a213cc7f826a9cb2e1388b10e32322ab246060a
k1adi <rizkiadinugroho3@gmail.com>
2024-06-06 14:30:49 +0700

    implementation collection agregate

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -448,4 +448,24 @@ class CollectionTest extends TestCase
         $result = $collection->reverse();
         $this->assertEqualsCanonicalizing([4,2,5,3,1], $result->all());
     }
+
+    public function testAggregate()
+    {
+        $collection = collect([1,2,3,4,5,6,7,8,9]);
+
+        $result = $collection->min();
+        $this->assertEquals(1, $result);
+
+        $result = $collection->max();
+        $this->assertEquals(9, $result);
+
+        $result = $collection->avg();
+        $this->assertEquals(5, $result);
+
+        $result = $collection->sum();
+        $this->assertEquals(45, $result);
+
+        $result = $collection->count();
+        $this->assertEquals(9, $result);
+    }
 }
